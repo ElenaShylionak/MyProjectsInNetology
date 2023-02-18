b1f2a06999782882ca7b80dd2fabb09aaee8937e
ElenaShylionak <99590421+ElenaShylionak@users.noreply.github.com>
2023-02-18 14:24:33 +0300

    Completing the task for the lesson Webinar on the topic "Creating an application for downloading photos from the NASA website"

---
+++ src/main/java/com/example/myprojectsinnetology/MyProjectsInNetologyApplication.java
@@ -0,0 +1,13 @@
+package com.example.myprojectsinnetology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectsInNetologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectsInNetologyApplication.class, args);
+    }
+
+}
